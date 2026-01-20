b512ffed334be86d56709c7dd673aca0c5acc4db
joke <jokezc@qq.com>
2026-01-20 22:47:44 +0800

    统一版本号格式去掉v

--- lib/core/constants/base_constants.dart
+++ lib/core/constants/base_constants.dart
@@ -2,7 +2,7 @@
 class BaseConstants {
   BaseConstants._();
   // 当前版本
-  static const String currentVersion = 'v1.0.0';
+  static const String currentVersion = '1.0.1';
   // 本地设备默认ID
   static const String webDevice = 'web_device';
   // 收藏歌单名称
